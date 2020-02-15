ea041dc87db2a5a92415618cc66889fe5f4cd430
Clay Taylor <claydevelops@gmail.com>
2020-02-15 12:54:32 -0500

    Testing on home pc

--- mad-libs-game/madlibsgame.py
+++ mad-libs-game/madlibsgame.py
@@ -2,6 +2,6 @@ color = input("Pick a color: ")
 plural_noun = input("Insert a plural noun: ")
 celebrity = input("Name a celebrity: ")
 
-print("Roses are " + color)
+print("Dandelions are " + color)
 print(plural_noun + " are blue")
 print("I love " + celebrity)
