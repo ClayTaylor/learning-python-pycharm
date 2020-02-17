c1587962ecebe0fab93667424c49611413f30fda
ClayTaylor <claydevelops@gmail.com>
2020-02-17 14:15:32 -0600

    Learning the Return Statement for a function

---
+++ return-statement/learning-return-statements.py
@@ -0,0 +1,14 @@
+
+def cube (num):
+    return num*num*num
+
+result = cube(4)
+print(result)
+
+# 1) Define the Function and its name (with parameters).
+# 2) Code what I want the function to do.
+# 3) Define what I want to be returned out of that function by using return.
+# 4) Print what was given back to me from the function.
+# 5) Once Python has reached the return in the function, anything else below it in the function will not print, effectively
+# "breaking me out" of the function and moving on down the code.
+# Side Note: Variables can be stored inside the function and can be called on outside of the function with pre-determined values.
