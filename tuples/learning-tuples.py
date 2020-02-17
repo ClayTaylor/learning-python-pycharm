405301a14b81b6a4426b98312d86fb03e3640797
ClayTaylor <claydevelops@gmail.com>
2020-02-17 11:49:55 -0600

    Learning about Tuples

---
+++ tuples/learning-tuples.py
@@ -0,0 +1,7 @@
+coordinates = (3, 7)
+# this can no longer be changed
+
+print(coordinates[1])
+
+# Tuples are used less often than lists. But are useful for storing data that will never change.
+# They are not to be confused with lists. Lists can have their data manipulated and changed.
