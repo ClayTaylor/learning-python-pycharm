2081b29ce315cbbd74c93fed4542d8e824bbabd7
Clay Taylor <claydevelops@gmail.com>
2020-02-19 23:34:36 -0500

    Learning Basics of For Loops

---
+++ for-loop/learning-for-loops.py
@@ -0,0 +1,9 @@
+
+# for letter in "Milkshake":
+  #  print(letter) // Will print out every letter in the string until it can no longer interate through the string.
+
+friends = ["Dalty", "Jordan", "Andrew"]
+for friend in friends:
+    print(friend)
+
+
